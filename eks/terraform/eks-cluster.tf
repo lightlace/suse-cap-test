e8882431c6f24ef86053008425c7f16ed1bd1d74
Farah Schueller <fschueller@suse.de>
2018-11-20 17:45:39 +0100

    Define rule for pod communication

--- eks/terraform/eks-cluster.tf
+++ eks/terraform/eks-cluster.tf
@@ -15,10 +15,20 @@ resource "aws_security_group" "eks-cluster" {
   }
 }
 
+resource "aws_security_group_rule" "eks-cluster-ingress-workers-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.eks-cluster.id}"
+  source_security_group_id = "${aws_security_group.eks-worker.id}"
+  to_port                  = 443
+  type                     = "ingress"
+}
+
 # The actual EKS resource
 
 resource "aws_eks_cluster" "eks-cluster" {
-  name            = "${var.cluster-name}-cluster"
+  name            = "${var.cluster-name}"
   role_arn        = "${aws_iam_role.eks-cluster.arn}"
 
   vpc_config {
